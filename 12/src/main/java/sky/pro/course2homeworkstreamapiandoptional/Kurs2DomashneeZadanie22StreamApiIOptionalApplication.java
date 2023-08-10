a132637ef51151d3e1317d5adcc85bd2f7daf1ac
unik0 <murotkafbi@gmail.com>
2023-08-10 23:05:35 +0300

    homework2.6

---
+++ 12/src/main/java/sky/pro/course2homeworkstreamapiandoptional/Kurs2DomashneeZadanie22StreamApiIOptionalApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homeworkstreamapiandoptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2DomashneeZadanie22StreamApiIOptionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2DomashneeZadanie22StreamApiIOptionalApplication.class, args);
+    }
+
+}
